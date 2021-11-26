8b53904d62d63e30dfa7619d6c14728b6815da0c
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-11-26 16:03:11 +0700

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,95 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  this.timeout(5000);
+  let likeGOOG = 0;
+  let likeMSFT = 0;
+  test('Viewing one stock', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=MSFT')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'MSFT');
+        likeMSFT = res.body.stockData.likes;
+        done();
+      });
+  });
+  test('Viewing one stock and liking it', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        likeGOOG = res.body.stockData.likes;
+        done();
+      });
+  });
+  test('Viewing the same stock and liking it again', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        done();
+      });
+  });
+  test('Viewing two stocks', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.property(res.body.stockData[0], 'stock');
+        assert.property(res.body.stockData[0], 'price');
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.equal(res.body.stockData.length, 2);
+        done();
+      });
+  });
+  test('Viewing two stocks and liking them', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.property(res.body.stockData[0], 'stock');
+        assert.property(res.body.stockData[0], 'price');
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.equal(res.body.stockData.length, 2);
+        done();
+      });
+  });
+  test('Likes number just change one time', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.likes, likeGOOG + 1);
+        done();
+      });
+  });
+  test('Check after liked two stocks', done => {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData[0].rel_likes, likeGOOG - likeMSFT);
+        done();
+      });
+  });
+  
 });
